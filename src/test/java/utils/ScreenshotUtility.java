e183c3c7be98531d06d1f055125c9b3090c5fefc
Suyog Nimbre <heisenbergnimbre@gmail.com>
2026-05-18 20:20:00 +0530

    Added Screenshot utility

--- src/test/java/utils/ScreenshotUtility.java
+++ src/test/java/utils/ScreenshotUtility.java
@@ -1,4 +1,22 @@
 package utils;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+import java.io.IOException;
+
 public class ScreenshotUtility {
+
+    public static void capture(WebDriver driver, String testName) throws IOException {
+
+        File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+        File dest = new File("screenshots/" + testName + ".png");
+
+        FileUtils.copyFile(src,dest);
+
+    }
 }
