c761a4ce05006e82b3281800220261e45ac84d51
Suyog Nimbre <heisenbergnimbre@gmail.com>
2026-05-30 19:33:06 +0530

    Updated ScreenShot utils

--- src/test/java/utils/ScreenshotUtility.java
+++ src/test/java/utils/ScreenshotUtility.java
@@ -7,16 +7,26 @@ import org.openqa.selenium.WebDriver;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class ScreenshotUtility {
 
-    public static void capture(WebDriver driver, String testName) throws IOException {
+    public static void capture(WebDriver driver, String testName)  {
 
-        File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"));
 
-        File dest = new File("screenshots/" + testName + ".png");
+        String filePath = "screenshots/" + testName + "_" + timestamp + ".png";
 
-        FileUtils.copyFile(src,dest);
+        try {
 
+            File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+            File dest = new File(filePath);
+
+            FileUtils.copyFile(src,dest);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
